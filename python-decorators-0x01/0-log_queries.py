508568fcb684230509b746efc5426fc6189d0b54
Halima Muktar <halimamuk1307@gmail.com>
2025-07-09 02:25:06 +0300

    Update 0-log_queries.py

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,13 +1,15 @@
 import sqlite3
 import functools
+from datetime import datetime  
 
-# Decorator to log SQL queries before execution
+# Decorator to log SQL queries with timestamp
 def log_queries(func):
     @functools.wraps(func)
     def wrapper_log(*args, **kwargs):
         query = kwargs.get('query') or (args[0] if args else None)
         if query:
-            print(f"[LOG] Executing SQL Query: {query}")
+            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            print(f"[{timestamp}] Executing SQL Query: {query}")
         else:
             print("[LOG] No SQL query provided")
         return func(*args, **kwargs)
