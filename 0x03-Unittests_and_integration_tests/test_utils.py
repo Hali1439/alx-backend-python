103d0ea2e712fa4c193153f128f428dd520232cd
Halima Muktar <halimamuk1307@gmail.com>
2025-07-18 02:24:31 +0300

    now

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -11,24 +11,23 @@ from utils import memoize
 class TestGetJson(unittest.TestCase):
     """Test get_json with mocked requests.get"""
 
-    @parameterized.expand(
-        [
-            (
-                "http://example.com",
-                {"payload": True},
-            ),
-            (
-                "http://holberton.io",
-                {"payload": False},
-            ),
-        ]
-    )
+    @parameterized.expand([
+        (
+            "http://example.com",
+            {"payload": True}
+        ),
+        (
+            "http://holberton.io",
+            {"payload": False}
+        ),
+    ])
     def test_get_json(self, test_url, test_payload):
         """Mock requests.get and test return of get_json"""
         mock_response = Mock()
         mock_response.json.return_value = test_payload
 
-        with patch("utils.requests.get", return_value=mock_response) as mock_get:
+        with patch("utils.requests.get",
+                   return_value=mock_response) as mock_get:
             result = get_json(test_url)
             mock_get.assert_called_once_with(test_url)
             self.assertEqual(result, test_payload)
@@ -56,3 +55,4 @@ class TestMemoize(unittest.TestCase):
             self.assertEqual(first, 42)
             self.assertEqual(second, 42)
             mocked.assert_called_once()
+
