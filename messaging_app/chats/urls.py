735db19073eb0df7a47493805dafc366cc4b962d
Halima Muktar <halimamuk1307@gmail.com>
2025-07-18 18:48:25 +0300

    py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework_nested.routers import NestedDefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
-# DRF router setup
-router = routers.DefaultRouter()
+# Base router
+router = NestedDefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
-router.register(r'messages', MessageViewSet, basename='message')
 
-# URLs generated from the router
+# Nested messages router
+convo_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
+convo_router.register(r'messages', MessageViewSet, basename='conversation-messages')
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(convo_router.urls)),
 ]
