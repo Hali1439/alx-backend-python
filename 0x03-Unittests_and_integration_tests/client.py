4fec389edfacae9ba9c789f3f77a80380e86a633
Halima Muktar <halimamuk1307@gmail.com>
2025-07-18 03:54:03 +0300

    now

--- 0x03-Unittests_and_integration_tests/client.py
+++ 0x03-Unittests_and_integration_tests/client.py
@@ -1,24 +1,58 @@
 #!/usr/bin/env python3
-"""GithubOrgClient module"""
+"""A github org client
+"""
+from typing import (
+    List,
+    Dict,
+)
 
-import requests
-
-
-def get_json(url):
-    """Fetches JSON data from the given URL"""
-    response = requests.get(url)
-    return response.json()
+from utils import (
+    get_json,
+    access_nested_map,
+    memoize,
+)
 
 
 class GithubOrgClient:
-    """Client class to interact with GitHub organization API"""
+    """A Githib org client
+    """
+    ORG_URL = "https://api.github.com/orgs/{org}"
 
-    ORG_URL = "https://api.github.com/orgs/{}"
+    def __init__(self, org_name: str) -> None:
+        """Init method of GithubOrgClient"""
+        self._org_name = org_name
 
-    def __init__(self, org_name):
-        self.org_name = org_name
+    @memoize
+    def org(self) -> Dict:
+        """Memoize org"""
+        return get_json(self.ORG_URL.format(org=self._org_name))
 
     @property
-    def org(self):
-        """Fetch organization data from GitHub"""
-        return get_json(self.ORG_URL.format(self.org_name))
+    def _public_repos_url(self) -> str:
+        """Public repos URL"""
+        return self.org["repos_url"]
+
+    @memoize
+    def repos_payload(self) -> Dict:
+        """Memoize repos payload"""
+        return get_json(self._public_repos_url)
+
+    def public_repos(self, license: str = None) -> List[str]:
+        """Public repos"""
+        json_payload = self.repos_payload
+        public_repos = [
+            repo["name"] for repo in json_payload
+            if license is None or self.has_license(repo, license)
+        ]
+
+        return public_repos
+
+    @staticmethod
+    def has_license(repo: Dict[str, Dict], license_key: str) -> bool:
+        """Static: has_license"""
+        assert license_key is not None, "license_key cannot be None"
+        try:
+            has_license = access_nested_map(repo, ("license", "key")) == license_key
+        except KeyError:
+            return False
+        return has_license
