5b11cef3849eb3fb7e3e7987728bbbfbf6849dd3
Halima Muktar <halimamuk1307@gmail.com>
2025-07-20 18:13:14 +0300

    Update fixtures.py

--- 0x03-Unittests_and_integration_tests/fixtures.py
+++ 0x03-Unittests_and_integration_tests/fixtures.py
@@ -1,18 +1,38 @@
 #!/usr/bin/env python3
-"""Fixtures for integration tests"""
+"""Fixtures for testing GithubOrgClient"""
 
 org_payload = {
     "login": "testorg",
-    "repos_url": "https://api.github.com/orgs/testorg/repos"
+    "id": 123456,
+    "url": "https://api.github.com/orgs/testorg",
+    "repos_url": "https://api.github.com/orgs/testorg/repos",
+    "description": "A test organization",
 }
 
 repos_payload = [
-    {"name": "repo1", "license": {"key": "apache-2.0"}},
-    {"name": "repo2", "license": {"key": "mit"}},
-    {"name": "repo3", "license": {"key": "apache-2.0"}},
+    {
+        "id": 1,
+        "name": "repo1",
+        "license": {"key": "apache-2.0"},
+    },
+    {
+        "id": 2,
+        "name": "repo2",
+        "license": {"key": "mit"},
+    },
+    {
+        "id": 3,
+        "name": "repo3",
+        "license": {"key": "apache-2.0"},
+    },
+    {
+        "id": 4,
+        "name": "repo4",
+        "license": None,
+    }
 ]
 
-expected_repos = ["repo1", "repo2", "repo3"]
+expected_repos = ["repo1", "repo2", "repo3", "repo4"]
 
 apache2_repos = ["repo1", "repo3"]
 
